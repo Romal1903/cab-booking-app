bfb0f9068a827e9f53461fe8ee8df632597628d0
Romal Tandel <romaltandel1903@gmail.com>
2026-03-08 17:39:04 +0530

    connect frontend to render backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://cab-booking-backend-di7d.onrender.com",
   withCredentials: true
 });
 
